2523240d292b4ed783509170270f2e9463503241
Mark-Kil <markkilcanarias@gmail.com>
2024-01-26 12:51:37 +0100

    table is ready

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import UserRow from '@/components/table/UserRow.vue';
 import { useUsersStore } from '@/stores/usersStore';
 
 const usersStore = useUsersStore()
@@ -7,10 +8,39 @@ const usersStore = useUsersStore()
 
 <template>
   <main>
-    <h1 v-if="usersStore.isLoaded">{{ usersStore.users[0].name }}</h1>
+    <h1>Users Database</h1>
+    <div>
+      <table v-if="usersStore.isLoaded">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>City</th>
+      </tr>
+      <UserRow v-for="user in usersStore.users" :key="user.id" :user="user"/>
+    </table>
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
 
+  main {
+    padding: 2%;
+  }
+
+  h1 {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 1.5rem;
+    text-align: center;
+    margin-bottom: 2%;
+  }
+
+  div {
+    display: flex;
+    justify-content: center;
+  }
+
+  th {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
 </style>
