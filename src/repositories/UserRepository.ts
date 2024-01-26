b8ee2f89410a12121e364de36a45c26b0150c6ce
Mark-Kil <markkilcanarias@gmail.com>
2024-01-26 09:26:57 +0100

    the repository is ready

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,17 @@
+export default class UserRepository {
+
+    url: string = "https://jsonplaceholder.typicode.com/users"
+
+    async getAll(): Promise<Array<Record<string, unknown>>>{
+
+        try {
+            const response = await fetch(this.url) 
+            const data = await response.json()
+            return data
+        } catch (error) {
+            throw new Error('Error occured during APIs users table data extraction')
+        }
+
+    }
+
+}
